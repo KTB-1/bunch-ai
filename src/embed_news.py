4604260ae9b4d53a825ba2b6cf9ee6a66a6e59a7
양태규 <yangtaegyu@yangtaegyuui-MacBookPro.local>
2024-08-29 02:30:11 +0900

    modify func to match DB

---
+++ src/embed_news.py
@@ -0,0 +1,106 @@
+import torch
+import chromadb
+from tqdm import tqdm
+from langchain_ollama import OllamaEmbeddings
+from langchain_chroma import Chroma
+import pandas as pd
+from chromadb.config import Settings
+from langchain_core.documents import Document
+from datetime import datetime
+from sqlalchemy import create_engine
+from dbconnect import create_connection_mariadb, get_embedding_zero_rows, get_and_update_embedding_zero_rows
+from dbconnect import get_userID_from_usernewsviews, get_user_news_views_data, insert_user_news_views_data
+from dbconnect import get_news_summaries_by_usernewsviews
+from database import get_decoded_summaries_modified_V1
+
+def get_data_and_store_chroma():
+    
+    summaries = get_decoded_summaries_modified_V1()
+
+    embedding_model = OllamaEmbeddings(model='gemma2:2b')
+
+    chroma_client = Chroma(
+        collection_name="news_embedding_testing_1",
+        embedding_function=embedding_model,
+        collection_metadata={"hnsw:space":"cosine"},
+        persist_directory="chroma_langchain_db",
+    )
+
+    # ChromaDB에 데이터 삽입
+    tmp_docus = []
+    for idx, row in enumerate(summaries):
+        sum_summary = row['summary']['point_1'] + ' ' + row['summary']['point_2'] + ' ' + row['summary']['point_3']
+        tmp = Document(
+            page_content=sum_summary,
+            metadata={
+                "news_id": str(row['news_id'])
+            },
+            id=int(row['news_id']),
+        )
+        tmp_docus.append(tmp)
+    chroma_client.add_documents(documents=tmp_docus)
+
+    # query = "비트코인 채굴업체들이 반감기 이후 수익 감소 문제를 해결하기 위해 AI 애플리케이션에 컴퓨팅 파워를 제공하는 새로운 사업 모델을 모색하고 있다. AI 시장의 성장과 함께, 비트코인 채굴업체들은 이미 가진 전력 및 데이터 센터 인프라를 활용하여 AI 수요에 부응할 수 있는 위치에 있다. 비트코인 채굴업체들은 AI 기반 사업으로 수익을 창출하며"
+    # results = chroma_client.similarity_search_with_score(query=query, k=5)
+
+    # # 결과 출력
+    # for i, (result, score) in enumerate(results, 1):
+    #     print(f"Article {i}:")
+    #     print(f"Score: {score}")
+    #     print(f"Description: {result.page_content}")
+    #     print(f"Metadata: {result.metadata}")
+    #     print()
+
+def recc_item(userid, cnt):
+
+    df = get_userID_from_usernewsviews(user_id=userid, k=cnt)
+    print(df)
+
+    result_list = get_news_summaries_by_usernewsviews(df)
+    print(result_list)
+
+    embedding_model = OllamaEmbeddings(model="gemma2:2b")
+
+    chroma_client = Chroma(
+        collection_name="news_embedding_testing_1",
+        embedding_function=embedding_model,
+        collection_metadata={"hnsw:space":"cosine"},
+        persist_directory="chroma_langchain_db",
+    )
+
+    for row in result_list:
+        query = row['summary']['point_1'] + ' ' + row['summary']['point_2'] + ' ' + row['summary']['point_3']
+        results = chroma_client.similarity_search_with_score(query=query, k=5)
+
+        # 4. 결과 출력
+        for i, (result, score) in enumerate(results, 1):
+            print(f"Article {i}:")
+            print(f"Score: {score}")
+            print(f"Description: {result.page_content}")
+            print(f"Metadata: {result.metadata}")
+            print()
+
+if __name__ == "__main__":
+    # get_data_and_store_chroma()
+    
+    recc_item(1, 5)
+
+    # df = get_embedding_zero_rows()
+    # print(df)
+
+    # if df is not None and len(df) > 0:
+    #     insert_user_news_views_data(df)
+    # else:
+    #     print("추가할 데이터가 없습니다.")
+
+# O # kaggle dataset으로 item-user matrix 계산 확인
+# O # gemma2 vs llama3.1 k=100으로 판단
+# mariaDB 연결 확인
+# flask로 api 만들기?
+# O # add texts => add docu
+# X # huggingface --> model 제한, fine-tuning한 모델 사용 불가
+# X # newsapi --> korean news api 지원 x
+# 개인 미션
+# Description 길이가 일정해야 가장 좋은 성능을 보임.
+# user_id와 news_id가 같은 경우, view_date를 update 해야할 듯
+# view_date, publication_date 형식 일정하게 하는게 좋음.
