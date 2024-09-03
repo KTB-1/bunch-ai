281fd7daefe105556a880c562f06a83239d14ddb
양태규 <yangtaegyu@yangtaegyuui-MacBookPro.local>
2024-09-03 10:54:01 +0900

    add httpclient

--- src/embed_news.py
+++ src/embed_news.py
@@ -13,6 +13,7 @@ from dbconnect import get_userID_from_usernewsviews, get_user_news_views_data, i
 from dbconnect import get_news_summaries_by_usernewsviews
 from database import get_decoded_summaries_modified_V1
 from datetime import datetime, timedelta
+from config import CHROMADB_HOST, CHROMADB_PORT
 
 def get_data_and_store_chroma():
     
@@ -109,11 +110,138 @@ def recc_item(userid, cnt):
 
     return recc_list
 
+def http_chroma():
+    summaries = get_decoded_summaries_modified_V1()
+
+    embedding_model = OllamaEmbeddings(model='gemma2:2b')
+
+    # HTTP 클라이언트 초기화
+    chroma_client = chromadb.HttpClient(
+        host=CHROMADB_HOST,  # 여기에 host를 설정합니다.
+        port=CHROMADB_PORT,
+        ssl=False,
+        settings=Settings(
+            chroma_api_impl="rest",  # REST API를 사용하도록 설정
+            anonymized_telemetry=False,  # 기타 설정 값들
+        ),
+    )
+
+    # 컬렉션 생성 또는 가져오기
+    collection = chroma_client.get_or_create_collection(
+        name="news_embedding_testing_0902",
+        # embedding_function=embedding_model,  # HTTP 클라이언트에서는 서버 측에서 임베딩을 처리합니다.
+        metadata={"hnsw:space": "cosine"},
+    )
+
+    # 문서 삽입 준비
+    ids = []
+    documents = []
+    metadatas = []
+    embeddings = []
+
+    for idx, row in enumerate(summaries):
+        sum_summary = row['summary']['point_1'] + ' ' + row['summary']['point_2'] + ' ' + row['summary']['point_3']
+        tmp_embedding = embedding_model.embed_query(sum_summary)
+
+        documents.append(sum_summary)
+        metadatas.append({"news_id": str(row['news_id'])})
+        ids.append(str(row['news_id']))
+        embeddings.append(tmp_embedding)
+
+    # 문서 삽입
+    collection.add(
+        documents=documents,
+        metadatas=metadatas,
+        ids=ids,
+        embeddings=embeddings
+    )
+
+    # query = "비트코인 채굴업체들이 반감기 이후 수익 감소 문제를 해결하기 위해 AI 애플리케이션에 컴퓨팅 파워를 제공하는 새로운 사업 모델을 모색하고 있다. AI 시장의 성장과 함께, 비트코인 채굴업체들은 이미 가진 전력 및 데이터 센터 인프라를 활용하여 AI 수요에 부응할 수 있는 위치에 있다. 비트코인 채굴업체들은 AI 기반 사업으로 수익을 창출하며"
+    query = "비트코인 채굴업체"
+    embedded_query = embedding_model.embed_query(query)
+    results = collection.query(
+        query_embeddings=[embedded_query],
+        n_results=5,
+        # where=,
+        # where_document=,
+    )
+
+    for i, text in enumerate(range(len(results['documents'][0]))):
+        print(results['documents'][0][i])
+
+def http_recc_item(userid, cnt):
+
+    df = get_userID_from_usernewsviews(user_id=userid, k=cnt)
+
+    ###
+    # view_date를 datetime 형식으로 변환
+    df['view_date'] = pd.to_datetime(df['view_date'], format='%Y-%m-%d %H:%M:%S')
+
+    # 현재 시간으로부터 5일 전의 날짜 계산
+    cutoff_date = datetime.now() - timedelta(days=10)
+
+    # 5일보다 오래된 날짜의 행 삭제
+    df = df[df['view_date'] >= cutoff_date]
+    ###
+
+    ### result_list 로 가져오는 개수 조정 (date 에 따라서)
+    result_list = get_news_summaries_by_usernewsviews(df)
+    result_list = result_list[:5]
+    # print(result_list)
+    
+    check_same = []
+    for it, tmp in enumerate(result_list):
+        check_same.append(tmp['news_id'])
+ 
+    embedding_model = OllamaEmbeddings(model="gemma2:2b")
+
+    # HTTP 클라이언트 초기화
+    chroma_client = chromadb.HttpClient(
+        host=CHROMADB_HOST,  # 여기에 host를 설정합니다.
+        port=CHROMADB_PORT,
+        ssl=False,
+        settings=Settings(
+            chroma_api_impl="rest",  # REST API를 사용하도록 설정
+            anonymized_telemetry=False,  # 기타 설정 값들
+        ),
+    )
+
+    # 컬렉션 생성 또는 가져오기
+    collection = chroma_client.get_or_create_collection(
+        name="news_embedding_testing_0902",
+        # embedding_function=embedding_model,  # HTTP 클라이언트에서는 서버 측에서 임베딩을 처리합니다.
+        metadata={"hnsw:space": "cosine"},
+    )
+
+    recc_list = []
+    for row in result_list:
+        sum_summary = row['summary']['point_1'] + ' ' + row['summary']['point_2'] + ' ' + row['summary']['point_3']
+        tmp_embedding = embedding_model.embed_query(sum_summary)
+        results = collection.query(
+            query_embeddings=[tmp_embedding],
+            n_results=5,
+            # where=,
+            # where_document=,
+        )
+        for item in results['ids'][0]:
+            recc_list.append(int(item))
+
+    recc_list = [item for item in recc_list if item not in check_same]
+    recc_list = list(set(recc_list))
+
+    return recc_list
+
 if __name__ == "__main__":
     # get_data_and_store_chroma()
     
-    recc_item(1, 3)
+    # recc_item(1, 3)
 
+    # http_chroma()
+
+    http_recc_item(1, 3)
+
+
+    ## 임의로 usernewsview 테이블에 데이터 집어넣기
     # df = get_embedding_zero_rows()
     # print(df)
 
