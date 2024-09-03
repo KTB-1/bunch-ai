1a347667c546bb8eccc2e068e4c2c1e20ed154a5
minji0916 <a2927036@naver.com>
2024-09-03 10:53:17 +0900

    refactor : .env 파일에는 api 키와 db설정만 남김

--- src/config.py
+++ src/config.py
@@ -1,25 +1,36 @@
 import logging
 from dotenv import load_dotenv
 import os
-import json
 import time
 
 # .env 파일 로드
 load_dotenv()
 
+# Naver news API 호출 시 설정
+# SEARCH_QUERIES=["주식", "채권", "금융", "금리", "환율 및 외환 시장", "금융 규제", "경제 지표"]
+# DISPLAY_COUNT=10
+# START_INDEX=1
+# END_INDEX=300
+# SORT_ORDER=sim
+SEARCH_QUERIES=["주식"]
+DISPLAY_COUNT=5
+START_INDEX=1
+END_INDEX=5
+SORT_ORDER='date'
+
+# 로그 설정
+LOG_FILE='news_project.log'
+
+# 성능 관련 설정
+CONCURRENCY=5  # 동시 실행 설정
+CHUNK_SIZE=30   # 청크 사이즈 설정
+TIMEOUT=30      # 타임아웃 설정 (초)
+MAX_RETRIES=2   # 최대 재시도 횟수
+RETRY_DELAY=1   # 재시도 대기 시간 (초)
+
 # 네이버 API의 클라이언트 ID와 시크릿 키 설정
 CLIENT_ID = os.getenv("CLIENT_ID")
 CLIENT_SECRET = os.getenv("CLIENT_SECRET")
-
-# Naver news API 호출 시 설정
-SEARCH_QUERIES = json.loads(os.getenv("SEARCH_QUERIES", '["주식", "채권"]'))
-DISPLAY_COUNT = int(os.getenv("DISPLAY_COUNT", 10))
-START_INDEX = int(os.getenv("START_INDEX", 1))
-END_INDEX = int(os.getenv("END_INDEX", 300))
-SORT_ORDER = os.getenv("SORT_ORDER", "sim")
-
-# 로그 설정
-LOG_FILE = os.getenv("LOG_FILE", 'news_project.log')
 
 def setup_logging():
     logging.basicConfig(
@@ -44,10 +55,3 @@ MYSQL_PORT = int(os.getenv("DB_PORT", 3306))
 MYSQL_USER = os.getenv("MYSQL_USER")
 MYSQL_PASSWORD = os.getenv("MYSQL_PASSWORD")
 MYSQL_DATABASE = os.getenv("MYSQL_DATABASE")
-
-# 성능 관련 설정
-CONCURRENCY = int(os.getenv("CONCURRENCY", 5))
-CHUNK_SIZE = int(os.getenv("CHUNK_SIZE", 30))
-TIMEOUT = int(os.getenv("TIMEOUT", 30))
-MAX_RETRIES = int(os.getenv("MAX_RETRIES", 2))
-RETRY_DELAY = int(os.getenv("RETRY_DELAY", 1))
