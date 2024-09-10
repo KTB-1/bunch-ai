82773df29acbb8da436ebf857de43e6063ffaf09
ShimHyesu <hesushim@gmail.com>
2024-09-10 10:41:36 +0900

    네트워크 수정

--- app-entrypoint.sh
+++ app-entrypoint.sh
@@ -6,11 +6,14 @@ MODEL_FILE_2="$MODEL_DIR/gemma2_2b"
 
 # OLLAMA_PORT 기본값 설정 (설정되지 않았을 경우)
 OLLAMA_PORT=${OLLAMA_PORT:-11434}
-echo "사용할 Ollama 포트: $OLLAMA_PORT"
+OLLAMA_HOST=${OLLAMA_HOST:-ollama}
+FULL_OLLAMA_HOST="${OLLAMA_HOST}:${OLLAMA_PORT}"
+
+echo "사용할 Ollama 호스트: $FULL_OLLAMA_HOST"
 
 # ollama 서버가 실행될 때까지 대기
 echo "ollama 서버 준비 중..."
-while ! curl -s http://ollama:$OLLAMA_PORT > /dev/null; do 
+while ! curl -s "http://$FULL_OLLAMA_HOST" > /dev/null; do 
   sleep 1
 done
 
@@ -19,7 +22,7 @@ echo "ollama 서버가 준비되었습니다."
 # 모델이 존재하는지 확인하고, 없으면 다운로드
 if [ ! -f "$MODEL_FILE_1" ]; then
     echo "모델 $MODEL_FILE_1이 존재하지 않습니다. 다운로드를 시작합니다."
-    ollama pull gemma2:latest
+    OLLAMA_HOST="$FULL_OLLAMA_HOST" ollama pull gemma2:latest
     if [ $? -ne 0 ]; then
         echo "gemma2:latest 모델 다운로드에 실패했습니다."
         exit 1
@@ -30,7 +33,7 @@ fi
 
 if [ ! -f "$MODEL_FILE_2" ]; then
     echo "모델 $MODEL_FILE_2이 존재하지 않습니다. 다운로드를 시작합니다."
-    ollama pull gemma2:2b
+    OLLAMA_HOST="$FULL_OLLAMA_HOST" ollama pull gemma2:2b
     if [ $? -ne 0 ]; then
         echo "gemma2:2b 모델 다운로드에 실패했습니다."
         exit 1
@@ -41,7 +44,6 @@ fi
 
 echo "모든 필요한 모델이 준비되었습니다."
 
-
 # 애플리케이션 실행
-echo "Starting the Python application..."
+echo "Python 애플리케이션을 시작합니다..."
 python3 src/main.py
