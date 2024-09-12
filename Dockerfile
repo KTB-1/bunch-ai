f31f2513884511d6c5a3bfe8fefee80edfcdf64d
ShimHyesu <hesushim@gmail.com>
2024-09-12 15:56:36 +0900

    Dockerfile 수정 : 하나의 컨테이너로 통합

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.11.9-slim
+FROM --platform=linux/amd64 python:3.11.9-slim
 
 WORKDIR /app
 
@@ -7,10 +7,9 @@ RUN apt-get update && \
     apt-get install -y gcc curl build-essential libreadline-dev && \
     apt-get clean && rm -rf /var/lib/apt/lists/*
 
-# curl 및 ollama 설치
-RUN curl -fsSL https://ollama.com/install.sh | sh
+RUN mkdir -p /app/models
 
-# Python 패키지 설치 (ChromaDB 포함)
+# Python 패키지 설치
 COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt
 
@@ -18,8 +17,8 @@ RUN pip install --no-cache-dir -r requirements.txt
 COPY ./src ./src
 
 # 애플리케이션 실행을 위한 entrypoint 스크립트 복사
-COPY app-entrypoint.sh /app-entrypoint.sh
-RUN chmod +x /app-entrypoint.sh
+COPY entrypoint.sh /entrypoint.sh
+RUN chmod +x /entrypoint.sh
 
 # 애플리케이션 실행
-CMD ["/app-entrypoint.sh"]
+CMD ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
 MODEL_DIR="/app/models"
-MODEL_FILE_1="$MODEL_DIR/gemma2_latest"
-MODEL_FILE_2="$MODEL_DIR/gemma2_2b"
+MODEL_FILE_1="$MODEL_DIR/gemma2/latest"
+MODEL_FILE_2="$MODEL_DIR/gemma2/2b"
 
-# OLLAMA_PORT 기본값 설정 (설정되지 않았을 경우)
-# OLLAMA_PORT=${OLLAMA_PORT:-11434}
-# OLLAMA_HOST=${OLLAMA_HOST:-ollama}
-FULL_OLLAMA_HOST="http://ollama:11434"
+# 환경 변수에서 OLLAMA_BASE_URL 설정을 가져옴
+OLLAMA_BASE_URL=${OLLAMA_BASE_URL:-"http://localhost:11434"}
 
-echo "사용할 Ollama 호스트: $FULL_OLLAMA_HOST"
+# ollama 서버를 백그라운드에서 실행
+echo "ollama 서버를 시작합니다..."
+OLLAMA_HOST="$OLLAMA_BASE_URL" ollama serve &
+
+echo "ollama 서버 백그라운드 실행"
 
 # ollama 서버가 실행될 때까지 대기
 echo "ollama 서버 준비 중..."
-while ! curl -s "$FULL_OLLAMA_HOST" > /dev/null; do 
+while ! curl -s "$OLLAMA_BASE_URL" > /dev/null; do 
   sleep 1
 done
 
@@ -22,7 +24,7 @@ echo "ollama 서버가 준비되었습니다."
 # 모델이 존재하는지 확인하고, 없으면 다운로드
 if [ ! -f "$MODEL_FILE_1" ]; then
     echo "모델 $MODEL_FILE_1이 존재하지 않습니다. 다운로드를 시작합니다."
-    OLLAMA_HOST="$FULL_OLLAMA_HOST" ollama pull gemma2:latest
+    OLLAMA_HOST="$OLLAMA_BASE_URL" ollama pull gemma2:latest
     if [ $? -ne 0 ]; then
         echo "gemma2:latest 모델 다운로드에 실패했습니다."
         exit 1
@@ -33,7 +35,7 @@ fi
 
 if [ ! -f "$MODEL_FILE_2" ]; then
     echo "모델 $MODEL_FILE_2이 존재하지 않습니다. 다운로드를 시작합니다."
-    OLLAMA_HOST="$FULL_OLLAMA_HOST" ollama pull gemma2:2b
+    OLLAMA_HOST="$OLLAMA_BASE_URL" ollama pull gemma2:2b
     if [ $? -ne 0 ]; then
         echo "gemma2:2b 모델 다운로드에 실패했습니다."
         exit 1
@@ -45,6 +47,6 @@ fi
 echo "모든 필요한 모델이 준비되었습니다."
 
 # 애플리케이션 실행
-echo "Python  ㅍ합니다..."
+echo "Python  실행합니다..."
 python3 src/app.py & 
 python3 src/main.py 
